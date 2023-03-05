c7a6d63b4b706309d5363a8a6254e28c53a712c8
Staro3mil <matachedavid@yahoo.com>
2023-03-05 12:12:49 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+This is a work in progress page that uses hashing to encrypt the data of its users (passwords). Once a user registers they will be able to see the secret. 
+
+Cookies are also enabled, this means that after a successful login or registration you can access the "/secrets" page without having to type in your credentials again.
+
+The "Submit" button is not yet developed.
+
+To start this page use "nodemon app.js" and then access localhost:3000
---
+++ app.js
@@ -0,0 +1,118 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const port = 3000;
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("passport-local-mongoose");
+
+const app = express();
+
+ 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb+srv://Stharon:Staro2mill@secrets.td67erw.mongodb.net/userDB?retryWrites=true&w=majority")
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocal);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", (req,res) => {
+    res.render("home");
+})
+
+
+app.get("/login", (req,res) => {
+    res.render("login");
+})
+
+app.get("/register", (req,res) => {
+    res.render("register");
+})
+
+app.get("/secrets", (req,res) => {
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+})
+
+app.get("/logout", (req,res) => {
+    req.logout((err)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+    
+})
+
+app.post("/register", (req,res) => {
+
+    User.register({username: req.body.username}, req.body.password, (err, user) =>{
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, ()=>{
+                res.redirect("/secrets");
+            });
+        }
+    });
+   
+});
+
+// app.post("/login", (req,res) => {
+
+//     const user = new User({
+//         username: req.body.username,
+//         password: req.body.password
+//     })
+    
+//     req.login(user, (err)=>{
+//         if (err){
+//             console.log(err);
+//         } else {
+//             passport.authenticate("local")(req, res, ()=> {
+//                 res.redirect("/secrets");
+//             })
+//         }
+//     })
+
+   
+// })
+
+app.post("/login", passport.authenticate("local"), function(req, res){
+    res.redirect("/secrets");
+});
+ 
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
